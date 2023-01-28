d75dc208f4ed7301d0e1c792f24568d2e8e9ba9d
Dragon-Knight <dubki4132@mail.ru>
2023-01-28 03:25:26 +0300

    BMSReader update

--- lib/L1_BMSReader/BMSReader.h
+++ lib/L1_BMSReader/BMSReader.h
@@ -5,177 +5,79 @@
 #pragma once
 
 #include <stdint.h>
-#include <cstring>
+#include <Arduino.h>
+#include <BMSPacket.h>
 
 class BMSReader
 {
 	public:
-		
-		#pragma pack(push, 1)
-		struct params_t
-		{
-			uint32_t header;            // Заголовок пакета.
-			uint16_t voltage;			// Напряжение на всей АКБ, мВ.
-			uint16_t voltage_1;         // Напряжение на 1 ячейки, мВ.
-			uint16_t voltage_2;         // Напряжение на 2 ячейки, мВ.
-			uint16_t voltage_3;         // Напряжение на 3 ячейки, мВ.
-			uint16_t voltage_4;         // Напряжение на 4 ячейки, мВ.
-			uint16_t voltage_5;         // Напряжение на 5 ячейки, мВ.
-			uint16_t voltage_6;         // Напряжение на 6 ячейки, мВ.
-			uint16_t voltage_7;         // Напряжение на 7 ячейки, мВ.
-			uint16_t voltage_8;         // Напряжение на 8 ячейки, мВ.
-			uint16_t voltage_9;         // Напряжение на 9 ячейки, мВ.
-			uint16_t voltage_10;        // Напряжение на 10 ячейки, мВ.
-			uint16_t voltage_11;        // Напряжение на 11 ячейки, мВ.
-			uint16_t voltage_12;        // Напряжение на 12 ячейки, мВ.
-			uint16_t voltage_13;        // Напряжение на 13 ячейки, мВ.
-			uint16_t voltage_14;        // Напряжение на 14 ячейки, мВ.
-			uint16_t voltage_15;        // Напряжение на 15 ячейки, мВ.
-			uint16_t voltage_16;        // Напряжение на 16 ячейки, мВ.
-			uint16_t voltage_17;        // Напряжение на 17 ячейки, мВ.
-			uint16_t voltage_18;        // Напряжение на 18 ячейки, мВ.
-			uint16_t voltage_19;        // Напряжение на 19 ячейки, мВ.
-			uint16_t voltage_20;        // Напряжение на 20 ячейки, мВ.
-			uint16_t voltage_21;        // Напряжение на 21 ячейки, мВ.
-			uint16_t voltage_22;        // Напряжение на 22 ячейки, мВ.
-			uint16_t voltage_23;        // Напряжение на 23 ячейки, мВ.
-			uint16_t voltage_24;        // Напряжение на 24 ячейки, мВ.
-			uint16_t voltage_25;        // Напряжение на 25 ячейки, мВ.
-			uint16_t voltage_26;        // Напряжение на 26 ячейки, мВ.
-			uint16_t voltage_27;        // Напряжение на 27 ячейки, мВ.
-			uint16_t voltage_28;        // Напряжение на 28 ячейки, мВ.
-			uint16_t voltage_29;        // Напряжение на 29 ячейки, мВ.
-			uint16_t voltage_30;        // Напряжение на 30 ячейки, мВ.
-			uint16_t voltage_31;        // Напряжение на 31 ячейки, мВ.
-			uint16_t voltage_32;        // Напряжение на 32 ячейки, мВ.
-			uint16_t reserved_70;		// 
-			int16_t current;            // Ток разряда иди заряда, мА.
-			uint8_t percent;            // Оставшаяся ёмкость АКБ, проценты.
-			uint32_t phy_capacity;      // Фактическая ёмкость АКБ, мА/ч.
-			uint32_t rem_capacity;      // Оставшаяся ёмкость АКБ, мА/ч.
-			uint32_t cycle_capacity;    // WAT?
-			uint32_t uptime;            // Кол-во времени с питанием, секунд.
-			int16_t temp_mos;           // Температура датчика MOSFET, Градусы.
-			int16_t temp_bal;           // Температура датчика Balance, Градусы.
-			int16_t temp_1;             // Температура датчика 1, Градусы.
-			int16_t temp_2;             // Температура датчика 2, Градусы.
-			int16_t temp_3;             // Температура датчика 3, Градусы.
-			int16_t temp_4;             // Температура датчика 4, Градусы.
-			uint8_t charge_fet;         // Флаг состояние ключа зарядки.
-			uint8_t dcharge_fet;        // Флаг состояние ключа разрядки.
-			uint8_t balanced;           // Флаг состояние балансировки.
-			uint16_t tire_length;       // WAT?
-			uint16_t pulses_num;        // WAT?
-			uint8_t relay;              // WAT?
-			uint32_t power;             // Текущая мощность, Вт.
-			uint8_t vmax_bat;           // Номер батарее с максимальный напряжением.
-			uint16_t vmax;              // Напряжение батареи с максимальным напряжением, мВ.
-			uint8_t vmin_bat;           // Номер батарее с минимальным напряжением.
-			uint16_t vin;               // Напряжение батареи с минимальным напряжением, мВ.
-			uint16_t vmid;              // Среднее напряжение батареи, мВ.
-			uint8_t wat_123;            // WAT?
-			uint16_t dcharge_fet_lost;  // Падение на транзисторе нагрузки, В.
-			uint16_t dcharge_fet_v;     // Напряжение на затворе транзистора нагрузки, В.
-			uint16_t charge_fet_v;      // Напряжение на затворе транзистора зарядки, В.
-			uint16_t wat_130131;        // WAT?
-			uint32_t balance_bits;      // Флаги сбалансированных ячеек.
-			uint16_t logs;              // WAT?
-			uint16_t crc;               // Контрольная сумма.
-		} params;
-		#pragma pack(pop)
-		
-		BMSReader()
-		{
-			Init();
 
+		using callback_t = void (*)(BMSPacket::params_t params);
+		
+		BMSReader(HardwareSerial &serial) : _serial(serial)
+		{
 			return;
 		}
 		
 		/*
 			Инициализация класса.
 		*/
-		void Init()
+		void Init(callback_t callback)
 		{
-			memset(&params, 0x00, sizeof(params));
-			_params_idx = 0;
-			_ready = false;
+			// Не получилось убрать HardwareSerial внутрь класса.
+			//HardwareSerial _serial(PB11, PB10);
+			//_serial.setRx(PB11);
+			//_serial.setTx(PB10);
+			_serial.begin(19200, SERIAL_8N1);
+			
+			_packet.Init();
+			
+			_callback = callback;
 			
 			return;
 		}
 		
 		/*
-			Пакет получен, распарсен, проверен и готов к выдаче данных.
+			Отправка запроса в BMS.
 		*/
-		bool IsReady()
+		void Request()
 		{
-			return _ready;
+			_serial.write(_packet.RequestData, sizeof(_packet.RequestData));
+			
+			return;
 		}
 		
 		/*
-			Вставить пакет по-байтно.
-			
-			@param data Байт данных
-			@return Кол-во оставшиеся байт до сбора пакета.
+			Проверяем ответ от BMS и вызываем callback.
 		*/
-		uint8_t PutByte(uint8_t data)
+		void Processing(uint32_t time)
 		{
-			if(_params_idx < sizeof(params))
+			
+			while( _serial.available() > 0 )
 			{
-				((uint8_t*) &params)[_params_idx++] = data;
-
-				if(_params_idx == sizeof(params))
+				uint8_t left = _packet.PutByte( _serial.read() );
+				if(left == 0)
 				{
-					if(_Check() == true)
+					if(_packet.IsReady() == true)
 					{
-						_Conversion();
+						_callback(_packet.params);
+						
+						_packet.Init();
+					}
+					else
+					{
+						// Переполнение.
 					}
 				}
 			}
 			
-			return sizeof(params) - _params_idx;
-		}
-		
-	private:
-		bool _Check()
-		{
-			bool result = false;
-			
-			if(params.header == 0xAA55AAFF)
-			{
-				if(params.crc == _CRC())
-				{
-					result = true;
-				}
-			}
-			
-			return result;
-		}
-
-		uint16_t _CRC()
-		{
-			uint16_t result = 0x0000;
-			
-			for(uint8_t i = 4; i < sizeof(params) - 2; ++i)
-			{
-				result += *((uint8_t*)(&params) + i);
-			}
-			
-			return result;
-		}
-		
-		/*
-			Метод конвертирует полученные данные в единый вид: Напряжение в мВ, Ток в А, время в Сек....
-			Модифицируется исходная структура, поэтому после crc будет не валидным.
-		*/
-		void _Conversion()
-		{
-			// ...
-			
-			_ready = true;
-			
 			return;
 		}
+	
+	private:
 		
-		uint8_t _params_idx;
-		bool _ready;
+		HardwareSerial &_serial;
+		BMSPacket _packet;
+		callback_t _callback;
+		
 };
--- src/BMSReader.cpp
+++ src/BMSReader.cpp
@@ -2,18 +2,37 @@
 #include <L2Wrapper.h>
 #include <BMSReader.h>
 
-BMSReader BMS;
+// Не получилось убрать HardwareSerial внутрь класса.
+HardwareSerial BMSSerial(PB11, PB10);
+BMSReader BMS(BMSSerial);
+
 L2Wrapper L2;
+
+void OnBMSData(BMSPacket::params_t params);
 
 void setup()
 {
-    
-    BMS.params.uptime;
+    Serial2.begin(115200, SERIAL_8N1);
+
+    BMS.Init(OnBMSData);
     
     return;
 }
 
 void loop()
 {
+    uint32_t time = millis();
+    
+    BMS.Processing(time);
+    
+    return;
+}
+
+void OnBMSData(BMSPacket::params_t params)
+{
+    Serial.print("Total volt: ");
+    Serial.print(params.voltage);
+    Serial.println("V;");
+    
     return;
 }
